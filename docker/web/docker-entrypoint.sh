00c8d2ac176c2d70804f895613073857d54b032d
fangnan <fangnan124@gmail.com>
2018-12-12 15:25:23 +0900

    Dockerize web & postgres

---
+++ docker/web/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+#if [ $RAILS_ENV = "production" ] || [ $RAILS_ENV = "staging" ]; then
+#  bundle install --without development test
+#  bundle exec rails assets:precompile
+#  yarn install --production
+#  # bundle exec rails webpacker:compile
+#else
+#  bundle install
+#  yarn install
+#fi
+#rm -f tmp/pids/server.pid
+#
+#if [ $RAILS_ENV = "development" ]; then
+#  bundle exec rails webpacker:clobber
+#fi
+#
+#if [ $RAILS_ENV = "production" ] || [ $RAILS_ENV = "staging" ]; then
+#  bundle exec rake sneakers:run &
+#  bundle exec sidekiq -e $RAILS_ENV -d -L log/sidekiq.log -C config/initializers/sidekiq.yml
+#fi
+
+bundle install
+yarn install
+bundle exec rails s -p 3000 -b '0.0.0.0'
+
+exec "$@"
